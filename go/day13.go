fc84089cb9ffd9d9b721bbbd7b94af93ed765512
Asher Vitek <asher.vitek@gmail.com>
2024-12-14 07:02:42 -0600

    Getting going on day13

--- go/day13.go
+++ go/day13.go
@@ -1,9 +1,48 @@
 package main
 
-func day13() int {
+import (
+	"strings"
+)
 
+type clawGame struct {
+	a     point
+	b     point
+	prize point
+}
 
+func day13(lines []string) int {
 
-	return 0
+    res := 0
+    game :=  clawGame{}
+	for _, line := range lines {
+		split := strings.Split(line, ": ")
 
+		switch split[0] {
+		case "Button A":
+			game.a = getButton(split[1])
+		case "Button B":
+			game.b = getButton(split[1])
+		case "Prize":
+			game.prize = getPrize(split[1])
+		case "":
+            res += runGame(game)
+		}
+	}
+
+	return res
+
+}
+
+func runGame(game clawGame) int {
+    return 0
+}
+
+func getButton(str string) point {
+
+	return point{}
+}
+
+func getPrize(str string) point {
+
+	return point{}
 }
