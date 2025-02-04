115314ee715bfa81c07450074502161af95ebe17
Asher Vitek <asher.vitek@gmail.com>
2025-02-04 13:26:42 -0600

    Day 19 done. needed to find some hints on this one and the answer was memoization which is something that I have not really done but have looked at with things like react and stuff like that. Did not make sense how it was working at first but then fiddled with this a little and now it makes sense how this works.

--- go/day19.go
+++ go/day19.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+var memory = make(map[string]int)
+
 func day19(lines []string) int {
 	patt, maxLen, towels := initDay19(lines)
 	results := 0
@@ -13,21 +15,45 @@ func day19(lines []string) int {
 
 	for _, towel := range towels {
 		fmt.Println(towel)
-		canMake(patt, maxLen, towel, &results)
+		// canMake(patt, maxLen, towel, &results)
+		results += part2(towel, patt)
 	}
 
 	return results
+}
+
+func part2(design string, towels map[string]bool) int {
+	if len(design) == 0 {
+		return 1
+	}
+
+	if memory[design] > 0 {
+		return memory[design]
+	}
+
+	result := 0
+	for towel := range towels {
+		if strings.Index(design, towel) == 0 {
+			remain := design[len(towel):]
+			result += part2(remain, towels)
+		}
+	}
+
+	memory[design] = result
+
+	return result
 }
 
 func canMake(patt map[string]bool, maxLen int, towel string, results *int) bool {
 
 	if len(towel) == 0 {
 		*results++
+		fmt.Println(*results)
 		return true
 	}
 
-	// for i := maxLen; i >= 1; i-- {
-	for i := 1; i <= maxLen; i++ {
+	for i := maxLen; i >= 1; i-- {
+		// for i := 1; i <= maxLen; i++ {
 		if len(towel) < i {
 			continue
 		}
@@ -39,7 +65,7 @@ func canMake(patt map[string]bool, maxLen int, towel string, results *int) bool 
 			continue
 		}
 
-		rest = strings.Replace(rest, pattern, "", -1)
+		// rest = strings.Replace(rest, pattern, "", -1)
 
 		if canMake(patt, maxLen, rest, results) {
 			continue
